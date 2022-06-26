31e7c4708b62803b3f5640dc2c25331b11e36914
ra54283suzuki <ra54283suzuki@gmail.com>
2022-06-26 22:41:10 +0900

    Apache install

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 resource "aws_instance" "example" {
   ami = "ami-0b7546e839d7ace12"
   instance_type = "t2.micro"
+  
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+  EOF
 }
